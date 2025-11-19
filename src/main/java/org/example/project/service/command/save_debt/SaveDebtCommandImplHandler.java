d4fbe5b841d43202aadf52e88d1c4d51100a3aa5
Dema-koder <asotruzikovic@gmail.com>
2025-11-19 20:17:34 +0300

    fix

--- src/main/java/org/example/project/service/command/save_debt/SaveDebtCommandImplHandler.java
+++ src/main/java/org/example/project/service/command/save_debt/SaveDebtCommandImplHandler.java
@@ -64,6 +64,6 @@ public class SaveDebtCommandImplHandler implements SaveDebtCommandHandler, Comma
 
     @Override
     public boolean canHandle(String command) {
-        return true;
+        return command.charAt(0) >= '0' && command.charAt(0) <= '9';
     }
 }
